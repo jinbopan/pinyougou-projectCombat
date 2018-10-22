b57620d6dddfa7b531cc10008c74b8b298554d32
liaojianbin <meinitcasthavefun@foxmail.com>
2018-10-22 12:06:16 +0800

    商家管理系统-动态认证授权类-数据库数据认证

--- pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/service/impl/UserDetailServiceImpl.java
+++ pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/service/impl/UserDetailServiceImpl.java
@@ -1,5 +1,8 @@
 package com.pinyougou.shop.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.pojo.TbSeller;
+import com.pinyougou.sellergoods.service.SellerService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +15,8 @@ import java.util.List;
 
 public class UserDetailServiceImpl implements UserDetailsService {
 
+    private SellerService sellerService;
+
     /**
      * 根据用户输入的用户名进行授权
      * @param username 用户输入的用户名
@@ -21,12 +26,22 @@ public class UserDetailServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        //角色权限集合（本来应该根据用户名到数据库中查询的，但是我们的系统没有角色表）
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        //指定一个角色叫ROLE_SELLER
-        authorities.add(new SimpleGrantedAuthority("ROLE_SELLER"));
+        //根据商家id到数据库中查询商家信息;如果存在并审核通过则返回用户信息，否则说明用户名不对登录失败
+        TbSeller seller = sellerService.findOne(username);
+        if (seller != null && "1".equals(seller.getStatus())) {
+            //角色权限集合（本来应该根据用户名到数据库中查询的，但是我们的系统没有角色表）
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            //指定一个角色叫ROLE_SELLER
+            authorities.add(new SimpleGrantedAuthority("ROLE_SELLER"));
 
-        //任意前端输入用户名；只要密码为123456的都认证通过
-        return new User(username, "123456", authorities);
+            //将用户在表单中输入的密码会与User中的密码进行对比，如果一致则登录成功，否则失败
+            return new User(username, seller.getPassword(), authorities);
+        }
+
+        return null;
+    }
+
+    public void setSellerService(SellerService sellerService) {
+        this.sellerService = sellerService;
     }
 }
