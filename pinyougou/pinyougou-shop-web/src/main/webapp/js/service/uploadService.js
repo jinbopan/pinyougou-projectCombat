3eb57eaaac8ea8a8b18b18037fb6a65ddb78593b
liaojianbin <meinitcasthavefun@foxmail.com>
2018-10-23 13:01:06 +0800

    商品图片上传

--- pinyougou/pinyougou-shop-web/src/main/webapp/js/service/uploadService.js
+++ pinyougou/pinyougou-shop-web/src/main/webapp/js/service/uploadService.js
@@ -1,7 +1,9 @@
 app.service("uploadService",function ($http) {
 
     this.uploadFile = function () {
+        //表单数据对象
         var formData = new FormData();
+        //添加一个表单项名称为file；
         formData.append("file", file.files[0]);
         return $http({
             url:"../upload.do",
