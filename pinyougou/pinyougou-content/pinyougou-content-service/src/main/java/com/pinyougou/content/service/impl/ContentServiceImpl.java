8dfa7a91637f58bc504478bf746012f716dd2b0f
liaojianbin <meinitcasthavefun@foxmail.com>
2018-10-29 11:59:56 +0800

    门户系统首页轮播广告数据缓存加载实现

--- pinyougou/pinyougou-content/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou/pinyougou-content/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -9,6 +9,7 @@ import com.pinyougou.content.service.ContentService;
 import com.pinyougou.service.impl.BaseServiceImpl;
 import com.pinyougou.vo.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
 import tk.mybatis.mapper.entity.Example;
 
@@ -19,6 +20,11 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements Co
 
     @Autowired
     private ContentMapper contentMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public static final String CONTENT = "content";
 
     @Override
     public PageResult search(Integer page, Integer rows, TbContent content) {
@@ -38,8 +44,18 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements Co
 
     @Override
     public List<TbContent> findContentListByCategoryId(Long categoryId) {
-
         List<TbContent> contentList = null;
+
+        try {
+            //从redis中查询是否存在该分类对应的数据有则返回
+            contentList = (List<TbContent>) redisTemplate.boundHashOps(CONTENT).get(categoryId);
+            if (contentList != null) {
+                return contentList;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         //根据内容分类（轮播广告）并且有效的内容数据按照排序字段降序排序
         //sql:select * from tb_content where category_id =? and status=1 order by sort_order desc
         Example example = new Example(TbContent.class);
@@ -52,6 +68,13 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent> implements Co
         example.orderBy("sortOrder").desc();
 
         contentList = contentMapper.selectByExample(example);
+
+        try {
+            //将分类对应的内容设置到redis中
+            redisTemplate.boundHashOps(CONTENT).put(categoryId, contentList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return contentList;
     }
