546ec3e98cee8345ee04657e63003b8ad8d23dc6
liaojianbin <meinitcasthavefun@foxmail.com>
2018-11-02 10:01:52 +0800

    FreeMarker常见指令

--- itcast-freemarker/src/main/resources/ftl/test.ftl
+++ itcast-freemarker/src/main/resources/ftl/test.ftl
@@ -5,6 +5,38 @@
     <title>Freemarker测试</title>
 </head>
 <body>
-${name}---${msg}
+${name}---${msg}<br>
+<hr>
+assign指令使用：指定一个变量，可以使用插值输出<br>
+<#-- assign的指令 -->
+<#assign linkman="黑马"/>
+${linkman}<br>
+<#assign info={"mobile":"13888888888", "address":"吉山村"} />
+mobile = ${info.mobile}；address = ${info.address}
+<br><hr><br>
+include指令：可以引入其他的模版<br>
+<#include "header.ftl"/>
+
+<br><hr><br>
+if条件控制语句<br>
+<#assign bool=true>
+
+<#if bool>
+    bool的值为true.
+<#else>
+    bool的值为false.
+</#if>
+
+<br><hr><br>
+list遍历集合<br>
+<#list goodsList as goods>
+    index = ${goods_index}---name=${goods.name}---price=${goods.price}<br>
+</#list>
+
+<br><hr><br>
+<br><hr><br>
+<br><hr><br>
+<br><hr><br>
+<br><hr><br>
 </body>
 </html>
--- itcast-freemarker/src/test/java/cn/itcast/freemarker/FreemarkerTest.java
+++ itcast-freemarker/src/test/java/cn/itcast/freemarker/FreemarkerTest.java
@@ -7,7 +7,9 @@ import org.junit.Test;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class FreemarkerTest {
@@ -29,6 +31,19 @@ public class FreemarkerTest {
         Map<String, Object> dataModel = new HashMap<>();
         dataModel.put("name", "传智播客");
         dataModel.put("msg", "can we chat");
+
+        //数据集合
+        List<Map<String, Object>> goodsList = new ArrayList<>();
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("name", "橘子");
+        map1.put("price", 3.0);
+        goodsList.add(map1);
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("name", "柚子");
+        map2.put("price", 6);
+        goodsList.add(map2);
+
+        dataModel.put("goodsList", goodsList);
 
         //4、输出；参数1：在模版中使用到的数据map；参数2：输出的媒介
         FileWriter fileWriter = new FileWriter("D:\\itcast\\test\\test.html");
