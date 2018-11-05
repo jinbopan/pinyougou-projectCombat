be95b149b7ebfd43b594132600c15990fcd188a3
liaojianbin <meinitcasthavefun@foxmail.com>
2018-11-05 09:58:18 +0800

    spring boot 配置修改-补充

--- itcast-spring-boot/src/main/java/cn/itcast/springboot/controller/HelloWorldController.java
+++ itcast-spring-boot/src/main/java/cn/itcast/springboot/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
-package cn.itcast.springboot;
+package cn.itcast.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    //也可以使用@Value
+
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/info")
     public String info(){
-        return "Hello SpringBoot.";
+        return "Hello SpringBoot." + environment.getProperty("url");
     }
 }
