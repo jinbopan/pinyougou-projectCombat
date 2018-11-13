af8a7e69b939b167c3382a5fe3c26b11a99e6edd
liaojianbin <meinitcasthavefun@foxmail.com>
2018-11-13 10:12:14 +0800

    秒杀系统首页商品数据动态加载

---
+++ pinyougou/pinyougou-seckill/pinyougou-seckill-interface/src/main/java/com/pinyougou/seckill/service/SeckillGoodsService.java
@@ -0,0 +1,19 @@
+package com.pinyougou.seckill.service;
+
+import com.pinyougou.pojo.TbSeckillGoods;
+import com.pinyougou.service.BaseService;
+import com.pinyougou.vo.PageResult;
+
+import java.util.List;
+
+public interface SeckillGoodsService extends BaseService<TbSeckillGoods> {
+
+    PageResult search(Integer page, Integer rows, TbSeckillGoods seckillGoods);
+
+
+    /**
+     * 查询状态为已审核，剩余库存大于0，大于等于开始时间，小于结束时间的那些秒杀商品并根据开始时间升序排序
+     * @return 秒杀商品列表
+     */
+    List<TbSeckillGoods> findList();
+}
---
+++ pinyougou/pinyougou-seckill/pinyougou-seckill-service/src/main/java/com/pinyougou/seckill/service/impl/SeckillGoodsServiceImpl.java
@@ -0,0 +1,64 @@
+package com.pinyougou.seckill.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.pinyougou.mapper.SeckillGoodsMapper;
+import com.pinyougou.pojo.TbSeckillGoods;
+import com.pinyougou.seckill.service.SeckillGoodsService;
+import com.pinyougou.service.impl.BaseServiceImpl;
+import com.pinyougou.vo.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.Date;
+import java.util.List;
+
+@Service(interfaceClass = SeckillGoodsService.class)
+public class SeckillGoodsServiceImpl extends BaseServiceImpl<TbSeckillGoods> implements SeckillGoodsService {
+
+    @Autowired
+    private SeckillGoodsMapper seckillGoodsMapper;
+
+    @Override
+    public PageResult search(Integer page, Integer rows, TbSeckillGoods seckillGoods) {
+        PageHelper.startPage(page, rows);
+
+        Example example = new Example(TbSeckillGoods.class);
+        Example.Criteria criteria = example.createCriteria();
+        /*if(!StringUtils.isEmpty(seckillGoods.get***())){
+            criteria.andLike("***", "%" + seckillGoods.get***() + "%");
+        }*/
+
+        List<TbSeckillGoods> list = seckillGoodsMapper.selectByExample(example);
+        PageInfo<TbSeckillGoods> pageInfo = new PageInfo<>(list);
+
+        return new PageResult(pageInfo.getTotal(), pageInfo.getList());
+    }
+
+    @Override
+    public List<TbSeckillGoods> findList() {
+        List<TbSeckillGoods> seckillGoodsList = null;
+
+        Example example = new Example(TbSeckillGoods.class);
+
+        Example.Criteria criteria = example.createCriteria();
+        //状态 已审核
+        criteria.andEqualTo("status", "1");
+        //库存大于0
+        criteria.andGreaterThan("stockCount", 0);
+
+        //开始时间小于等于当前时间
+        criteria.andLessThanOrEqualTo("startTime", new Date());
+        //结束时间大于当前时间
+        criteria.andGreaterThan("endTime", new Date());
+
+        //根据开始时间升序排序
+        example.orderBy("startTime");
+
+        seckillGoodsList = seckillGoodsMapper.selectByExample(example);
+
+        return seckillGoodsList;
+    }
+}
